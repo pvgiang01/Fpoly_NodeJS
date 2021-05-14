f9958b84b90269ebae7c930072c082023d374437
channn3 <channn3@fpt.edu.vn>
2021-05-14 09:38:57 +0700

    next

---
+++ React_Native/Demo001/App.js
@@ -0,0 +1,24 @@
+
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+
+// ES6
+// react-native init PS12345_NguyenVanA_Lab01
+// react-native run-android
+// npx react-native run-android
+
+const App = () => {
+
+  return (
+    <SafeAreaView >
+      <Text>PS12345 Nguyen van A</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
